520d6ebd111f0dd329f7b0ced703cd78d55f6718
MonsieurMeh <monsieurmeh@devnullsoftware.com>
2025-09-19 23:11:05 -0700

    feat: paint manager

---
+++ Managers/PaintManagers/BasePaintManager.cs
@@ -0,0 +1,145 @@
+using UnityEngine;
+
+
+namespace ExpandedAiFramework
+{
+    public enum PaintMode : int
+    {
+        Inactive = 0,
+        Active = 1
+    }
+
+    public abstract class BasePaintManager : ILogInfoProvider
+    {
+        protected EAFManager mManager;
+        protected PaintMode mCurrentPaintMode = PaintMode.Inactive;
+        protected GameObject mPaintMarker = null;
+        protected Vector3 mPaintMarkerPosition = Vector3.zero;
+        protected Quaternion mPaintMarkerRotation = Quaternion.identity;
+
+        public abstract string TypeName { get; }
+        public virtual string InstanceInfo { get { return string.Empty; } }
+        public abstract string TypeInfo { get; }
+        public bool IsActive => mCurrentPaintMode == PaintMode.Active;
+
+        public BasePaintManager(EAFManager manager)
+        {
+            mManager = manager;
+        }
+
+        public virtual void Initialize() { }
+        public virtual void Shutdown() { CleanupPaintMarker(); }
+        public virtual void Update() { UpdatePaintMarker(); }
+
+        public abstract void StartPaint(string[] args);
+        public abstract void ExitPaint();
+        public abstract void ProcessCommand(string command, string[] args);
+
+        public virtual void HandlePaintInput()
+        {
+            if (mCurrentPaintMode != PaintMode.Active) return;
+
+            bool shiftPressed = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+            
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (shiftPressed)
+                {
+                    HandleShiftLeftClick();
+                }
+                else
+                {
+                    HandleLeftClick();
+                }
+            }
+            else if (Input.GetMouseButtonDown(1))
+            {
+                if (shiftPressed)
+                {
+                    HandleShiftRightClick();
+                }
+                else
+                {
+                    HandleRightClick();
+                }
+            }
+        }
+
+        protected virtual void HandleLeftClick() { }
+        protected virtual void HandleShiftLeftClick() { }
+        protected virtual void HandleRightClick() { ExitPaint(); }
+        protected virtual void HandleShiftRightClick() { ExitPaint(); }
+
+        protected virtual void UpdatePaintMarker()
+        {
+            if (mCurrentPaintMode == PaintMode.Inactive || GameManager.m_vpFPSCamera.m_Camera == null)
+            {
+                CleanupPaintMarker();
+                return;
+            }
+
+            HandlePaintInput();
+            UpdatePaintMarkerInternal();
+        }
+
+        protected virtual void UpdatePaintMarkerInternal()
+        {
+            Ray ray = GameManager.m_vpFPSCamera.m_Camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, float.MaxValue, Utils.m_PhysicalCollisionLayerMask))
+            {
+                mPaintMarkerPosition = hit.point;
+                if (mPaintMarker != null && mPaintMarker.transform != null)
+                {
+                    mPaintMarker.transform.position = hit.point;
+                }
+            }
+        }
+
+        protected virtual void CleanupPaintMarker()
+        {
+            if (mPaintMarker != null)
+            {
+                UnityEngine.Object.Destroy(mPaintMarker);
+                mPaintMarker = null;
+            }
+        }
+
+        protected GameObject CreateMarker(Vector3 position, Color color, string name, float height, float diameter = 5f)
+        {
+            GameObject waypointMarker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            UnityEngine.Object.Destroy(waypointMarker.GetComponent<Collider>());
+            waypointMarker.transform.localScale = new Vector3(diameter, height, diameter);
+            waypointMarker.transform.position = position;
+            waypointMarker.GetComponent<Renderer>().material.color = color;
+            waypointMarker.name = name;
+            GameObject waypointTopMarker = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            UnityEngine.Object.Destroy(waypointTopMarker.GetComponent<Collider>());
+            waypointTopMarker.transform.localScale = new Vector3(diameter * 3f, diameter * 3f, diameter * 3f);
+            waypointTopMarker.transform.position = position + new Vector3(0, height, 0);
+            waypointTopMarker.GetComponent<Renderer>().material.color = color;
+            waypointTopMarker.name = name + "Top";
+            waypointTopMarker.transform.SetParent(waypointMarker.transform);
+            return waypointMarker;
+        }
+
+        protected GameObject ConnectMarkers(Vector3 pos1, Vector3 pos2, Color color, string name, float height, float diameter = 5f)
+        {
+            GameObject waypointConnector = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            UnityEngine.Object.Destroy(waypointConnector.GetComponent<Collider>());
+            Vector3 direction = pos1 - pos2;
+            float distance = direction.magnitude;
+            waypointConnector.transform.position = (pos1 + pos2) / 2.0f + new Vector3(0f, height, 0f);
+            waypointConnector.transform.localScale = new Vector3(diameter, distance / 2.0f, diameter);
+            waypointConnector.transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
+            waypointConnector.GetComponent<Renderer>().material.color = color;
+            waypointConnector.name = name;
+            return waypointConnector;
+        }
+
+        protected void Teleport(Vector3 position, Quaternion rotation)
+        {
+            GameManager.GetPlayerTransform().position = position + new Vector3(0f, 1f, 0f);
+            GameManager.GetPlayerTransform().rotation = rotation;
+        }
+    }
+}
---
+++ Managers/PaintManagers/MapDataPaintManager.cs
@@ -0,0 +1,278 @@
+using UnityEngine;
+
+
+namespace ExpandedAiFramework
+{
+    public abstract class MapDataPaintManager<T> : BasePaintManager where T : class, IMapData, new()
+    {
+        protected string mCurrentDataName = string.Empty;
+        protected string mCurrentDataNameBase = string.Empty;
+        protected string mCurrentDataPath = string.Empty;
+        protected string mDefaultDataPath = string.Empty;
+        protected List<GameObject> mDebugShownObjects = new List<GameObject>();
+        protected DataManager DataManager => mManager.DataManager;
+
+        public override string TypeInfo => $"MapDataPaintManager<{typeof(T).Name}>";
+
+        public MapDataPaintManager(EAFManager manager) : base(manager) 
+        {
+            mDefaultDataPath = Path.Combine(GetDataFolderPath(), $"{typeof(T).Name}s.json");
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            mCurrentDataPath = mDefaultDataPath;
+        }
+
+        public override void ProcessCommand(string command, string[] args)
+        {
+            switch (command.ToLower())
+            {
+                case CommandString_Delete: ProcessDelete(args); break;
+                case CommandString_Save: ProcessSave(args); break;
+                case CommandString_Load: ProcessLoad(args); break;
+                case CommandString_GoTo: ProcessGoTo(args); break;
+                case CommandString_Show: ProcessShow(args); break;
+                case CommandString_Hide: ProcessHide(args); break;
+                case CommandString_List: ProcessList(args); break;
+                case CommandString_Paint: ProcessPaint(args); break;
+                case CommandString_Set: ProcessSet(args); break;
+                default: this.LogWarningInstanced($"Unknown command: {command}"); break;
+            }
+        }
+
+        public virtual void ProcessSet(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                this.LogWarningInstanced("Set command requires property and value");
+                return;
+            }
+            
+            string property = args[0].ToLower();
+            string value = args[1];
+            
+            switch (property)
+            {
+                case "datapath": 
+                    mCurrentDataPath = value;
+                    this.LogAlwaysInstanced($"Set data path to: {value}");
+                    break;
+                default:
+                    ProcessSetCustom(property, value);
+                    break;
+            }
+        }
+
+        protected virtual void ProcessSetCustom(string property, string value)
+        {
+            this.LogWarningInstanced($"Unknown property: {property}");
+        }
+
+        protected abstract void ProcessDelete(string[] args);
+        protected abstract void ProcessGoTo(string[] args);
+        protected abstract void ProcessPaint(string[] args);
+
+        protected virtual void ProcessSave(string[] args)
+        {
+            DataManager.SaveMapData();
+        }
+
+        protected virtual void ProcessLoad(string[] args)
+        {
+            DataManager.LoadMapData();
+        }
+
+        protected virtual void ProcessShow(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                ShowAll();
+            }
+            else
+            {
+                ShowByName(args[0]);
+            }
+        }
+
+        protected virtual void ProcessHide(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                HideAll();
+            }
+            else
+            {
+                HideByName(args[0]);
+            }
+        }
+
+        protected virtual void ProcessList(string[] args)
+        {
+            DataManager.ScheduleMapDataRequest<T>(new ForEachMapDataRequest<T>(mManager.CurrentScene, (data) =>
+            {
+                this.LogAlwaysInstanced($"Found {data}. Occupied: {data.Claimed}");
+            }, false));
+        }
+
+        protected abstract void ShowAll();
+        protected abstract void ShowByName(string name);
+        protected abstract void HideAll();
+        protected abstract void HideByName(string name);
+
+        protected void GetUniqueMapDataName(string baseName, Action<string> callback)
+        {
+            DataManager.ScheduleMapDataRequest<T>(new GetUniqueMapDataNameRequest<T>(mManager.CurrentScene, baseName, callback));
+        }
+
+        protected void GetMapDataByName(string name, Action<T, RequestResult> callback)
+        {
+            DataManager.ScheduleMapDataRequest<T>(new GetMapDataByNameRequest<T>(name, mManager.CurrentScene, callback, false));
+        }
+
+        protected void RegisterMapData(T data, Action<T, RequestResult> callback)
+        {
+            DataManager.ScheduleMapDataRequest<T>(new RegisterDataRequest<T>(data, mCurrentDataPath, callback, false));
+        }
+
+        protected void DeleteMapData(Guid guid, Action<T, RequestResult> callback)
+        {
+            DataManager.ScheduleMapDataRequest<T>(new DeleteDataRequest<T>(guid, mManager.CurrentScene, callback, false));
+        }
+
+        protected void ForEachMapData(Action<T> callback)
+        {
+            DataManager.ScheduleMapDataRequest<T>(new ForEachMapDataRequest<T>(mManager.CurrentScene, callback, false));
+        }
+
+        protected virtual string GetDataFolderPath()
+        {
+            return Path.Combine(MelonLoader.Utils.MelonEnvironment.ModsDirectory, DataFolderPath);
+        }
+
+        protected virtual bool IsNameProvided(string name, bool required = true)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                if (required)
+                {
+                    this.LogWarningInstanced("Name is required but not provided");
+                }
+                return false;
+            }
+            return true;
+        }
+
+        private class ForEachMapDataRequest<TData> : DataRequest<TData> where TData : IMapData, new()
+        {
+            protected Action<TData> mForEachCallback;
+            protected string mScene;
+
+            public override string TypeInfo => $"ForEachMapDataRequest<{typeof(TData).Name}>";
+
+            public ForEachMapDataRequest(string scene, Action<TData> forEachCallback, bool callbackIsThreadSafe) 
+                : base(null, true, callbackIsThreadSafe)
+            {
+                mForEachCallback = forEachCallback;
+                mScene = scene;
+            }
+
+            protected override bool Validate()
+            {
+                if (mForEachCallback == null)
+                {
+                    this.LogTraceInstanced("null foreach callback");
+                    return false;
+                }
+                return true;
+            }
+
+            protected override RequestResult PerformRequestInternal()
+            {
+                try
+                {
+                    foreach (TData data in mDataContainer.GetSceneData(mScene).Values)
+                    {
+                        if (mThreadSafeCallback)
+                        {
+                            mForEachCallback.Invoke(data);
+                        }
+                        else
+                        {
+                            TData dispatchedData = data;
+                            EAFManager.Instance.DispatchManager.Dispatch(() => mForEachCallback.Invoke(dispatchedData));
+                        }
+                    }
+                    return RequestResult.Succeeded;
+                }
+                catch (Exception e)
+                {
+                    this.LogErrorInstanced(e.Message);
+                    return RequestResult.Failed;
+                }
+            }
+        }
+
+        private class GetUniqueMapDataNameRequest<TData> : Request<TData> where TData : IMapData, new()
+        {
+            protected string mScene;
+            protected string mBaseName;
+            protected Action<string> mFoundNameCallback;
+
+            public override string TypeInfo => $"GetUniqueMapDataNameRequest<{typeof(TData).Name}>";
+
+            public GetUniqueMapDataNameRequest(string scene, string baseName, Action<string> callback) 
+                : base(null, true, false)
+            {
+                mBaseName = baseName;
+                mFoundNameCallback = callback;
+                mScene = scene;
+            }
+
+            protected override bool Validate()
+            {
+                if (mFoundNameCallback == null)
+                {
+                    this.LogTraceInstanced("null foundname callback");
+                    return false;
+                }
+                if (string.IsNullOrEmpty(mScene))
+                {
+                    this.LogTraceInstanced("null or empty scene");
+                    return false;
+                }
+                if (string.IsNullOrEmpty(mBaseName))
+                {
+                    this.LogTraceInstanced("null or empty mBaseName");
+                    return false;
+                }
+                return true;
+            }
+
+            protected override RequestResult PerformRequestInternal()
+            {
+                try
+                {
+                    int counter = 1;
+                    while (counter < 1000)
+                    {
+                        if (mDataContainer.GetSceneData(mScene).Values.Any(s => s.Name == $"{mBaseName}_{counter}"))
+                        {
+                            counter++;
+                            continue;
+                        }
+                        mFoundNameCallback.Invoke($"{mBaseName}_{counter}");
+                        return RequestResult.Succeeded;
+                    }
+                    return RequestResult.Failed;
+                }
+                catch (Exception e)
+                {
+                    this.LogErrorInstanced(e.Message);
+                    return RequestResult.Failed;
+                }
+            }
+        }
+    }
+}
