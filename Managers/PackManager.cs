e7aa425c41a8bc7d5aad410f6452e6f6fa58133f
MonsieurMeh <mensoirmeh@gmail.com>
2025-10-14 21:38:04 -0700

    feat: tick rate; more uproots; helper methods

--- Managers/PackManager.cs
+++ Managers/PackManager.cs
@@ -11,8 +11,11 @@ namespace ExpandedAiFramework
 {
     public class PackManager : BaseSubManager, IPackManager
     {
-        private const long TickRate = 500000L; // 10,000 ticks per ms, 1,000ms per second = 10,000,000 factor
+        private const LogCategoryFlags UpdateFlags = LogCategoryFlags.PackManager | LogCategoryFlags.Update;
+        private const long TickRate = MillisecondsToTicks * 50;
         private bool mStartCalled = false;
+        private long mStartTick = 0;
+        private long mDeserializeDelayTick = 0;
         private long mNextTick = 0;
         private PackSettings mPackSettings;
         private VanillaPackManager mVanillaManager;
@@ -66,8 +69,10 @@ namespace ExpandedAiFramework
                 panel.QuietlyResetTimberWolfCombatMusic();
             }
             VanillaPackManager.m_HoursPlayedAtStart = GameManager.GetTimeOfDayComponent().GetHoursPlayedNotPaused();
-            VanillaPackManager.m_SystemEnabled = VanillaPackManager.ArePacksAllowed();
-            mPackSettings = VanillaPackManager.GetPackSettings();
+            VanillaPackManager.m_SystemEnabled = ArePacksAllowed();
+            mPackSettings = GetPackSettings();
+            mStartTick = DateTime.Now.Ticks;
+            mDeserializeDelayTick = DateTime.Now.Ticks + (long)(VanillaPackManager.m_DeserializeDelayTimer * MillisecondsToTicks);
         }
 
 
@@ -79,9 +84,9 @@ namespace ExpandedAiFramework
             if (!ShouldUpdate()) return;
             MaybeEnableAnimalsOnLoad();
 			ResetGroupEventFlags();
-			MaybeCleanupDeadPackAnimals(mPackSettings);
-			VanillaPackManager.MaybeDisbandGroupOnTargetLost(mPackSettings);
-			VanillaPackManager.MaybeFleeAndDisbandOnMoraleCheck();
+			MaybeCleanupDeadPackAnimals();
+			MaybeDisbandGroupOnTargetLost();
+			MaybeFleeAndDisbandOnMoraleCheck();
 			VanillaPackManager.MaybeFleeAndDisbandOnBearOrMooseCheck(mPackSettings);
 			VanillaPackManager.MaybeForceMoveMembers(mPackSettings);
 			VanillaPackManager.MaybeMoveToNewHoldGroundPosition(mPackSettings);
@@ -94,11 +99,31 @@ namespace ExpandedAiFramework
 
         private bool ShouldUpdate()
         {
-            if (VanillaPackManager == null) return false;
-            if (!VanillaPackManager.m_SystemEnabled) return false;
-            if (GameManager.m_IsPaused) return false;
-            if (GameManager.s_IsGameplaySuspended) return false;
-            if (GameManager.ControlsLocked()) return false;
+            if (VanillaPackManager == null) 
+            {
+                LogTrace($"VanillaPackManager is null", UpdateFlags);
+                return false;
+            }
+            if (!VanillaPackManager.m_SystemEnabled) 
+            {
+                LogTrace($"VanillaPackManager is not enabled", UpdateFlags);
+                return false;
+            }
+            if (GameManager.m_IsPaused) 
+            {
+                LogTrace($"Game is paused", UpdateFlags);
+                return false;
+            }
+            if (GameManager.s_IsGameplaySuspended) 
+            {
+                LogTrace($"Game is paused", UpdateFlags);
+                return false;
+            }
+            if (GameManager.ControlsLocked()) 
+            {
+                LogTrace($"Controls are locked", UpdateFlags);
+                return false;
+            }
             return true;
         }
 
@@ -117,8 +142,16 @@ namespace ExpandedAiFramework
 
         private void EnablePackAnimal(PackAnimal animal)
         {
-            if (animal.isActiveAndEnabled) return;
-            if (animal.transform.parent == null) return; 
+            if (animal.isActiveAndEnabled)
+            {
+                LogTrace($"Pack animal is already active", UpdateFlags);
+                return;
+            }
+            if (animal.transform.parent == null) 
+            {
+                LogTrace($"Pack animal has no parent", UpdateFlags);
+                return;
+            }
             animal.gameObject.SetActive(value: true);
             if (animal.transform.parent.gameObject.activeSelf) return; 
             animal.transform.parent.gameObject.SetActive(value: true);
@@ -132,19 +165,19 @@ namespace ExpandedAiFramework
             }
         }
 
-        private void MaybeCleanupDeadPackAnimals(PackSettings settings)
+        private void MaybeCleanupDeadPackAnimals()
         {
-            foreach (PackAnimal animal in GetPossiblyDeadPackAnimals(settings))
+            foreach (PackAnimal animal in EnumeratePackAnimals())
             {
-                if (IsPackAnimalDead(animal, settings)) continue;
+                if (IsPackAnimalDead(animal)) continue;
                 VanillaPackManager.UnregisterPackAnimal(animal, true);
             }
         }
 
 
-        private IEnumerable<PackAnimal> GetPossiblyDeadPackAnimals(PackSettings settings)
+        private IEnumerable<PackAnimal> EnumeratePackAnimals()
         {
-            foreach (PackGroup packGroup in VanillaPackManager.m_PackAnimalGroupByLeader.Values)
+            foreach (PackGroup packGroup in EnumeratePackGroups())
             {
                 foreach (PackAnimal animal in packGroup.m_Members)
                 {
@@ -153,14 +186,185 @@ namespace ExpandedAiFramework
             }
         }
 
-
-        private bool IsPackAnimalDead(PackAnimal animal, PackSettings settings) 
+        private IEnumerable<PackGroup> EnumeratePackGroups()
         {
-            if (animal.m_BaseAi.GetAiMode() == AiMode.Dead) return true;
-            if (animal.m_BaseAi.IsBleedingOut() && animal.m_BaseAi.GetBleedingOutMinutesRemaining() < settings.m_MinBleedOutTimeMinutes) return true;
-            if (!animal.isActiveAndEnabled && animal.m_BaseAi.HasUpdated()) return true; // This one may need special EAF attention, m_BaseAi.HasUpdated() is unlikely to work properly...
+            foreach (PackGroup packGroup in VanillaPackManager.m_PackAnimalGroupByLeader.Values)
+            {
+                yield return packGroup;
+            }
+        }
+
+        private IEnumerable<PackAnimal> EnumeratePackLeaders()
+        {
+            foreach (PackAnimal key in VanillaPackManager.m_PackAnimalGroupByLeader.Keys)
+            {
+                yield return key;
+            }
+        }
+
+
+        private bool IsPackAnimalDead(PackAnimal animal) 
+        {
+            if (animal.m_BaseAi.GetAiMode() == AiMode.Dead) 
+            {
+                LogTrace($"Pack animal is dead", UpdateFlags);
+                return true;
+            }
+            if (animal.m_BaseAi.IsBleedingOut() && animal.m_BaseAi.GetBleedingOutMinutesRemaining() < mPackSettings.m_MinBleedOutTimeMinutes) 
+            {
+                LogTrace($"Pack animal is bleeding out", UpdateFlags);
+                return true;
+            }
+            if (!animal.isActiveAndEnabled && animal.m_BaseAi.HasUpdated()) 
+            {
+                LogTrace($"Pack animal is not active and enabled and has updated", UpdateFlags);
+                return true;
+            }
             return false;
         }
+
+
+        private void MaybeDisbandGroupOnTargetLost()
+        {
+            if (mDeserializeDelayTick > DateTime.Now.Ticks) 
+            {
+                LogTrace($"Deserialize delay tick is greater than current time tick, skipping", UpdateFlags);
+                return;
+            }
+            float hoursPlayedNotPaused = GameManager.GetTimeOfDayComponent().GetHoursPlayedNotPaused();
+            foreach (PackAnimal leader in EnumeratePackLeaders())
+            {
+                PackGroup pack = VanillaPackManager.m_PackAnimalGroupByLeader[leader];
+                foreach (PackAnimal animal in pack.m_Members)
+                {
+                    if (animal.m_BaseAi.m_CurrentTarget == null) continue;
+                    pack.m_TargetAwarenessTime = hoursPlayedNotPaused;
+                    break;
+                }
+                float dispandTimper = IsTaggedGroupAnimal(leader) 
+                    ? mPackSettings.m_DisbandTaggedGroupOnForgetTargetAfterTimeHours 
+                    : mPackSettings.m_DisbandGroupOnForgetTargetAfterTimeHours;
+                if (hoursPlayedNotPaused - pack.m_TargetAwarenessTime <= dispandTimper) 
+                {
+                    LogTrace($"Hours played not paused minus target awareness time is less than dispand timer, skipping", UpdateFlags);
+                    continue;
+                }
+                RemoveGroup(leader, setReformTimer: true);
+            }
+        }
+
+
+        private void MaybeFleeAndDisbandOnMoraleCheck()
+        {
+            foreach (PackAnimal leader in EnumeratePackLeaders())
+            {
+                PackGroup pack = VanillaPackManager.m_PackAnimalGroupByLeader[leader];
+                foreach (PackAnimal animal in pack.m_Members)
+                {
+                    if (!ShouldAnimalFlee(animal, mPackSettings)) continue;
+                    LogTrace($"ShouldAnimalFlee returned true for animal with hash {animal.m_BaseAi.GetHashCode()}", UpdateFlags);
+                    foreach (PackAnimal member in pack.m_Members)
+                    {
+                        member.m_BaseAi.FleeFrom(GameManager.GetPlayerTransform().position); // TODO: Handle with EAF
+                        member.m_BaseAi.SetFleeReason(AiFleeReason.PackDisband); // TODO: Handle with EAF
+                        MaybeSetAuroraMaterialsOnFlee(member.m_BaseAi); // TODO: Handle with EAF
+                    }
+                    GameManager.GetAchievementManagerComponent().DefeatTimberwolfPack();
+                    RemoveGroup(leader, true);
+                    break;
+                }
+            }
+        }
+
+
+        private bool ShouldAnimalFlee(PackAnimal animal, PackSettings settings)
+        {
+            if (animal == null) 
+            {
+                LogTrace($"Null PackAnimal", UpdateFlags);
+                return false;
+            }
+            if (!animal.m_GroupLeader) 
+            {
+                LogTrace($"Not group leader", UpdateFlags);
+                return false;
+            }
+            if (IsMismatchWildlifeMode(animal)) 
+            {
+                LogTrace($"Mismatch wildlife mode", UpdateFlags);
+                return true;
+            }
+            if (GameManager.GetPlayerManagerComponent().m_ForceAIFlee)
+            {
+                LogTrace($"Force AIFlee", UpdateFlags);
+                return true;
+            }
+            float fleeMoraleThreshold = settings.m_FleeMoraleThreshold / 100f;
+            float groupMoraleHeuristic = GroupMoraleHeuristic(animal);
+            if (groupMoraleHeuristic < fleeMoraleThreshold) 
+            {
+                LogTrace($"Group morale heuristic less than flee morale threshold", UpdateFlags);
+                return true;
+            }
+            if (!IsTaggedGroupAnimal(animal)) 
+            {
+                LogTrace($"Not tagged group animal", UpdateFlags);
+                return false;
+            }
+            if (VanillaPackManager.m_PackAnimalGroupByLeader.Count == 0) 
+            {
+                LogTrace($"No pack animal group by leader", UpdateFlags);
+                return false;
+            }
+            PackAnimal groupLeader = animal.m_GroupLeader;
+            int packSize = VanillaPackManager.m_PackAnimalGroupByLeader[groupLeader].m_Members.Count;
+            bool flag = true;
+            foreach (PackAnimal leader in EnumeratePackLeaders())
+            {
+                if (groupLeader == leader)
+                {
+                    LogTrace($"Group leader is the same as the leader, setting flag false", UpdateFlags);
+                    flag = false;
+                    continue;
+                }
+                float otherLeaderHeuristic = GroupMoraleHeuristic(leader);
+                if (otherLeaderHeuristic > groupMoraleHeuristic) 
+                {
+                    LogTrace($"Other leader heuristic greater than group morale heuristic", UpdateFlags);
+                    return true;
+                }
+                if (!(otherLeaderHeuristic < groupMoraleHeuristic))
+                {
+                    PackGroup otherPack = VanillaPackManager.m_PackAnimalGroupByLeader[leader];
+                    if (otherPack.m_Members.Count > packSize) 
+                    {
+                        LogTrace($"Other pack members count greater than pack size", UpdateFlags);
+                        return true;
+                    }
+                    if (otherPack.m_Members.Count >= packSize && flag) 
+                    {
+                        LogTrace($"Other pack members count greater than or equal to pack size and flag is true", UpdateFlags);
+                        return true;
+                    }
+                }
+            }
+            LogTrace($"???bruh???", UpdateFlags);
+            return false;
+        }
+
+
+        #region eventual uproots
+        
+        private float GroupMoraleHeuristic(PackAnimal animal) => VanillaPackManager.GroupMoraleHeuristic(animal);
+        private bool IsMismatchWildlifeMode(PackAnimal animal) => VanillaPackManager.IsMismatchWildlifeMode(animal);
+        private bool IsTaggedGroupAnimal(PackAnimal animal) => VanillaPackManager.IsTaggedGroupAnimal(animal);
+        private void MaybeSetAuroraMaterialsOnFlee(BaseAi baseAi) => VanillaPackManager.MaybeSetAuroraMaterialsOnFlee(baseAi);
+        private void RemoveGroup(PackAnimal leader, bool setReformTimer) => VanillaPackManager.RemoveGroup(leader, setReformTimer);
+        private bool ArePacksAllowed() => VanillaPackManager.ArePacksAllowed();
+        private PackSettings GetPackSettings() => VanillaPackManager.GetPackSettings();
+
+        #endregion
+
 
 
         #region overrides
