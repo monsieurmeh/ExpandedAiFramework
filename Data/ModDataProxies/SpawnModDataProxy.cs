dfbeb95d57206758c56f2f121ca9634fa3e54270
MonsieurMeh <monsieurmeh@gmail.com>
2025-12-01 15:46:15 -0800

    feat: add ILogInfoProvider to SpawnModDataProxy and add targetting logging for type resolution backup

--- Data/ModDataProxies/SpawnModDataProxy.cs
+++ Data/ModDataProxies/SpawnModDataProxy.cs
@@ -4,7 +4,7 @@ using MelonLoader.TinyJSON;
 
 namespace ExpandedAiFramework
 {
-    public class SpawnModDataProxy : ModDataProxy
+    public class SpawnModDataProxy : ModDataProxy, ILogInfoProvider
     {
         [Exclude] private Type mVariantSpawnType;
         [Exclude] public bool Disconnected = false;
@@ -31,6 +31,8 @@ namespace ExpandedAiFramework
         public SpawnModDataProxy() : base() { }
 
         public override string DisplayName { get { return $"{VariantSpawnTypeString}-{mGuid}";  } }
+        public string InstanceInfo { get { return DisplayName; } }
+        public string TypeInfo { get { return "SpawnModDataProxy"; } }
 
 
 
@@ -60,7 +62,7 @@ namespace ExpandedAiFramework
             {
                 return true;
             }
-            var type = Type.GetType(VariantSpawnTypeString);
+            Type type = Type.GetType(VariantSpawnTypeString);
             if (type != null)
             {
                 mVariantSpawnType = type;
@@ -69,13 +71,15 @@ namespace ExpandedAiFramework
             string[] parts = VariantSpawnTypeString.Split(',');
             if (parts.Length < 2)
             {
-                LogError($"Could not parse type string {VariantSpawnTypeString} during SpawnModDataProxy.InitializeType()!");
+                this.LogErrorInstanced($"Could not parse type string {VariantSpawnTypeString} during SpawnModDataProxy.InitializeType()!");
                 return false;
             }
             string fullName = parts[0].Trim();
             string assemblyName = parts[1].Trim();
+            this.LogDebugInstanced($"Checking for {fullName} from assembly {assemblyName}", LogCategoryFlags.SerializedData | LogCategoryFlags.Ai);
             foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
             {
+                this.LogDebugInstanced($"Checking assembly: {assembly.FullName}", LogCategoryFlags.SerializedData | LogCategoryFlags.Ai);
                 if (assembly.GetName().Name != assemblyName)
                 {
                     continue;
@@ -86,9 +90,10 @@ namespace ExpandedAiFramework
                     continue;
                 }
                 mVariantSpawnType = type;
+                this.LogDebugInstanced($"Type found and resolved! Name: {mVariantSpawnType.Name}", LogCategoryFlags.Ai | LogCategoryFlags.SerializedData);
                 return true;
             }
-            LogError($"Unable to resolve type: {VariantSpawnTypeString} during SpawnModDataProxy.InitializeType()!");
+            this.LogErrorInstanced($"Unable to resolve type: {VariantSpawnTypeString} during SpawnModDataProxy.InitializeType()!");
             return false;
         }
 
@@ -120,7 +125,7 @@ namespace ExpandedAiFramework
                 CurrentRotation = baseAi.transform.rotation;
             } catch (Exception e)
             {
-                LogWarning($"Failed to save position and rotation during SpawnModDataProxy serialization; possibly this creature is despawned currently? ({e})");
+                this.LogWarningInstanced($"Failed to save position and rotation during SpawnModDataProxy serialization; possibly this creature is despawned currently? ({e})");
             }
         }
     }
