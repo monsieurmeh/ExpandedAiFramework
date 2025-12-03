fd34d69a9a12a08532c88f14d686e68cbd34ebfe
MonsieurMeh <monsieurmeh@gmail.com>
2025-12-02 18:22:08 -0800

    fix: encourage bears to spawn at their den

--- CustomBaseAi/BaseBear.cs
+++ CustomBaseAi/BaseBear.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using UnityEngine;
 
 namespace ExpandedAiFramework
 {
@@ -7,5 +7,14 @@ namespace ExpandedAiFramework
     {
         public static BaseBearSettings BaseBearSettings;
         public BaseBear(IntPtr ptr) : base(ptr) { }
+
+        public override void Initialize(BaseAi ai, TimeOfDay timeOfDay, SpawnRegion spawnRegion, SpawnModDataProxy proxy)
+        {
+            base.Initialize(ai, timeOfDay, spawnRegion, proxy);
+            if (!proxy.Fresh) return;
+            if (spawnRegion.m_Den == null) return;
+            if (!AiUtils.GetClosestNavmeshPos(out Vector3 denPos, spawnRegion.m_Den.transform.position, spawnRegion.m_Den.transform.position)) return;
+            ai.transform.position = denPos;
+        }
     }
 }
